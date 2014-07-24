d99c96d018e370613b4666a9b3dfcc8f05b9ca59
Mitja <mitjanikolaus@gmail.com>
2014-07-24 15:01:39 +0200

    removing more old ruby versions

--- scripts/install_omf6_rc_ubuntu.sh
+++ scripts/install_omf6_rc_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function removeOldRuby {
- apt-get -y --purge remove ruby-rvm ruby ruby1.9.3
+ apt-get -y --purge remove ruby-rvm ruby ruby1.8 ruby1.8-dev ruby1.9.3
  rm -rf /usr/share/ruby-rvm /etc/rvmrc /etc/profile.d/rvm.sh
  apt-get -y autoremove
 }
